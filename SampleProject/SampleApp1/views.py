c359cbb5d245a316ecf08e2d2865085bf780e84b
sivakarnati <sivakarnati45@gmail.com>
2024-10-31 18:58:46 +0530

    Update views.py my_update

--- SampleProject/SampleApp1/views.py
+++ SampleProject/SampleApp1/views.py
@@ -6,3 +6,6 @@ def f11(request):
 	return HttpResponse("<h1>Hello from SampleApp1 f11()</h1><hr />"); 
 def f22(request): 
 	return HttpResponse("<h1>Hello from SampleApp1 f22()</h1><hr />");
+
+def f33(request):
+	return HttpResponse("<h2> I am the new Developer </h2><hr/>");
